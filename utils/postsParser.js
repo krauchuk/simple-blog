71b281259c3debef9ea40a3b8dfc0f2b72fed23b
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2023-06-01 19:17:48 +0200

    added dynamic routes

--- utils/postsParser.js
+++ utils/postsParser.js
@@ -4,16 +4,35 @@ import matter from 'gray-matter'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
+const getId = fileName => fileName.replace(/\.md$/, '')
+
 export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map(fileName => {
-    const id = fileName.replace(/\.md$/, '')
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
     const matterResult = matter(fileContents)
 
-    return { id, ...matterResult.data }
+    return { id: getId(fileName), ...matterResult.data }
   })
 
   return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1))
+}
+
+export function getAllPostIds() {
+  return fs.readdirSync(postsDirectory).map(fileName => {
+    return {
+      params: {
+        id: getId(fileName),
+      },
+    }
+  })
+}
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const matterResult = matter(fileContents)
+
+  return { id, ...matterResult.data }
 }
