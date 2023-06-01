25d541d16e8b2aa6c97d322443efb2c887fcfce2
Ihar Krauchuk <ihar.krauchuk@gmail.com>
2023-06-01 20:34:32 +0200

    fix date

--- components/LocalDate/index.jsx
+++ components/LocalDate/index.jsx
@@ -7,7 +7,7 @@ const localFormat = {
 export const LocalDate = ({ dateString }) => {
   const splitted = dateString.split('-')
   const date = new Date(Date.UTC(splitted[0], splitted[1], splitted[2]))
-  const formatted = date.toLocaleDateString(undefined, localFormat)
+  const formatted = date.toLocaleDateString('en-GB', localFormat)
 
   return <time dateTime={dateString}>{formatted}</time>
 }
